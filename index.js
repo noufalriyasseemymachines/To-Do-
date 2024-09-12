19855a7c83cbe596cb92d5b83e4be7d09c04b646
noufalriyas <akshay@seemymachines.com>
2024-09-12 10:48:49 +0530

    to-do updated

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const year = today.getFullYear();
 const month = String(today.getMonth() + 1).padStart(2, '0'); 
 const day = String(today.getDate()).padStart(2, '0');
 const formattedDate = `${year}-${month}-${day}`;
-console.log(formattedDate)
 
 const alertMessage=document.getElementById("alert-message");
 
